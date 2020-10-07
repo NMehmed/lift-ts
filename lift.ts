59412c33d0210f81b3ecf5d59e82705ec6e9e491
NMehmed <nedzhati.mehmed@holidayextras.com>
2020-10-07 20:49:19 +0300

    set main points on lift class

--- lift.ts
+++ lift.ts
@@ -1,13 +1,54 @@
 class Lift {
-  queues: Array<number>
+  queues: Array<Array<number>>
   capacity: number
-  baseFloor: number
-  history: Array<number>
+  history: Array<number> = []
+  isMainCourseUp: boolean = true
+  isPassengersGoingUp: boolean = true
+  peopleIn: Array<number> = []
+  currentFloor: number = 0
 
-  constructor(queues: Array<number>, capacity: number) {
+  constructor(queues: Array<Array<number>>, capacity: number) {
     this.queues = queues
     this.capacity = capacity
-    this.baseFloor = 0
-    this.history = []
+  }
+
+  public start() {
+    while (this.queues.some(queue => queue.length > 0) || this.peopleIn.length > 0) {
+      this.history.push(this.currentFloor)
+
+      this.getOffPeople()
+      this.getPeopleIn()
+
+      if (this.shouldChangeMainCourse()) this.changeMainCourse()
+
+      this.setNextStop()
+    }
+  }
+
+  private getPeopleIn() {
+    const passengers = this.queues[this.currentFloor].filter(x => this.isMainCourseUp ? x > this.currentFloor : x < this.currentFloor)
+    const peopleWhoCanGetInCount = this.getFreeSpace() > passengers.length ? passengers.length : this.getFreeSpace()
+
+    for (let i = 0; i < peopleWhoCanGetInCount; i++) {
+      this.peopleIn.push(passengers[i])
+    }
+  }
+
+  private getOffPeople() {
+    this.peopleIn = this.peopleIn.filter(x => x !== this.currentFloor)
+  }
+
+  private shouldChangeMainCourse(): boolean {
+    return false
+  }
+
+  private changeMainCourse() {
+  }
+
+  private setNextStop() {
+  }
+
+  private getFreeSpace(): number {
+    return this.capacity - this.peopleIn.length
   }
 }
